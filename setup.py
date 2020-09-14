3c0f45a145842666728decd194be06e4dbf1b956
requires.io <support@requires.io>
2020-09-15 01:11:38 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -26,4 +26,4 @@ setup(
         'Programming Language :: Python :: 2.7'],
     keywords='pootle index search elasticsearch haystack',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    install_requires=['django-haystack==2.8.1', 'pootle'])
+    install_requires=['django-haystack==3.0', 'pootle'])
